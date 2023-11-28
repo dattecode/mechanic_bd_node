5388abb46d3ba3a4713378afa20431f1378da68e
dattecode <dattecode@gmail.com>
2023-11-27 22:31:48 -0300

    uld

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const repairsRoute = require("./repairs/repairs.rout")
 //app 
 const app = express()
 app.use(express.json())
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded());
 
 //rutas
 app.use("/api/v1/repairs", repairsRoute)
